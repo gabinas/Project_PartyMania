66cd68f5e9f1631b8da120e0bfbecfb552b82845
Gabriela Lugo <gabrielalugobaez@hotmail.com>
2018-11-07 20:41:20 -0800

    Added New Party Activity

---
+++ app/src/main/java/ca/ubco/cosc341/project_partymania/NewParty.java
@@ -0,0 +1,15 @@
+package ca.ubco.cosc341.project_partymania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewParty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_party);
+    }
+
+
+}
