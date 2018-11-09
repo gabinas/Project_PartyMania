093e10dee06d8e7bd1d96f096a8973806bb5d2d8
tafhim <tafhim_chelsea@live.co.uk>
2018-11-08 19:55:39 -0800

    test

---
+++ app/src/main/java/ca/ubco/cosc341/project_partymania/CurrentParty.java
@@ -0,0 +1,13 @@
+package ca.ubco.cosc341.project_partymania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentParty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_party);
+    }
+}
