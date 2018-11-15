21809fa0f3ef42e6f9d28304daa4da5278172223
Gabriela Lugo <gabrielalugobaez@hotmail.com>
2018-11-14 19:07:38 -0800

    Hamburguer manu added

--- app/src/main/java/ca/ubco/cosc341/project_partymania/MainActivity.java
+++ app/src/main/java/ca/ubco/cosc341/project_partymania/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.ubco.cosc341.project_partymania;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createParty(View view){
+        Intent intent = new Intent(MainActivity.this, NewParty.class);
+        startActivity(intent);
     }
 }
