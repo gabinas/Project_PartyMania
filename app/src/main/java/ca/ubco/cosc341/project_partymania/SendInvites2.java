04e7915c4e7e0625cd960726619025e71fd6797d
gabinas <gabrielalugobaez@hotmail.com>
2018-11-21 00:50:31 -0800

    Changes by Adrian that I upload cause he has git problems

---
+++ app/src/main/java/ca/ubco/cosc341/project_partymania/SendInvites2.java
@@ -0,0 +1,13 @@
+package ca.ubco.cosc341.project_partymania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendInvites2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_invites2);
+    }
+}
